10a92020bfcfed0ad9d8e3690234d900efa20064
Shankar Ambady <shanbady@gmail.com>
2017-02-07 20:09:27 -0500

    adding working build

--- ansible/roles/django-gunicorn/files/entrypoint.sh
+++ ansible/roles/django-gunicorn/files/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
-cd /wwwroot
+cd /wwwroot/app
+ls -la /wwwroot
 echo "waiting on mysql"
 while ! mysqladmin ping -h"mysql" -u --silent 2>/dev/null; do
 	sleep 3
 	echo "..."
 done
-export DJANGO_ALLOWED_HOSTS='*'
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 gunicorn --log-file=- --workers 4 --name impact_gunicorn -b 0.0.0.0:8000 impact.wsgi:application
